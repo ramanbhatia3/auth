b159045ac20b2314505404b2503103ada69eb290
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-09-04 23:19:14 +0530

    feat: implement user login with password verification

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,3 +32,31 @@ export const signup = async (req, res) => {
         return res.status(500).json({ message : "Server Error" })
     }
 }
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body
+
+        if (!email || !password){
+            return res.status(400).json({ message : "All fields are required" })
+        }
+
+        const user = await User.findOne({ email })
+
+        if (!user){
+            return res.status(401).json({ message : "Invalid email or password" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch){
+            return res.status(401).json({ message : "Invalid email or password" })
+        }
+
+        return res.status(200).json({ message : "LogIn Successful" })
+    }
+    catch(error){
+        console.error("LogIn Error: ",error)
+        return res.status(500).json({ message : "Server Error" })
+    }
+}
