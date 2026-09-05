225d4ed7b949a146f968012ae667b0a3dad2b998
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-09-05 22:19:47 +0530

    feat: add JWT authentication middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -74,3 +74,23 @@ export const login = async (req, res) => {
         return res.status(500).json({ message : "Server Error" })
     }
 }
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId).select('-password')
+
+        if (!user){
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        return res.status(200).json({ user })
+    }
+    catch (error){
+        console.error("Get Me Error: ", error)
+        return res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
